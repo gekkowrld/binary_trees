365dfb171b1afc54e2cd11c2936dd3032f70e670
Gekko Wrld <gekkowrld@gmail.com>
2024-03-27 01:05:55 +0300

    doc: Add documentation in accordance to ALX
    Signed-off-by: Gekko Wrld <gekkowrld@gmail.com>

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,12 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_t - Create a new node
+ * @parent: The parent node
+ * @value: The value to be inserted
+ *
+ * Return: Return a binary node
+*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 		binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
